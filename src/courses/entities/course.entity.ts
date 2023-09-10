75e1ce1c9888091f8e6c6219ea4f3f74f14bcbf2
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-09-10 12:17:13 -0300

    refactor: courses and tags entities

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -4,13 +4,18 @@ import {
   Column,
   JoinTable,
   ManyToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
 } from 'typeorm';
 import { Tag } from './tag.entity';
 
+import { v4 as uuidv4 } from 'uuid';
+
 @Entity('courses')
 export class Course {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
   name: string;
@@ -18,7 +23,23 @@ export class Course {
   @Column()
   description: string;
 
-  @JoinTable()
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @BeforeInsert()
+  generatedId() {
+    if (this.id) return;
+    this.id = uuidv4();
+  }
+
   @ManyToMany(() => Tag, (tag) => tag.courses, { cascade: true })
+  @JoinTable({
+    name: 'courses_tags',
+    joinColumn: { name: 'course_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'tag_id', referencedColumnName: 'id' },
+  })
   tags: Tag[];
 }
--- src/courses/entities/tag.entity.ts
+++ src/courses/entities/tag.entity.ts
@@ -1,14 +1,32 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  CreateDateColumn,
+  BeforeInsert,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
 import { Course } from './course.entity';
 
 @Entity('tags')
 export class Tag {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
   name: string;
 
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
   @ManyToMany(() => Course, (course) => course.tags)
   courses: Course[];
+
+  @BeforeInsert()
+  generatedId() {
+    if (this.id) return;
+    this.id = uuidv4();
+  }
 }
