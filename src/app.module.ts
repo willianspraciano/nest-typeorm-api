04dc2271963520df9e9d3316fc721917ba45b5b6
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-09-12 10:09:31 -0300

    refactor: use configModule on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [ConfigModule.forRoot(), CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/data-source.config.ts
+++ src/config/data-source.config.ts
@@ -1,23 +1,20 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
-import { ConfigService } from '@nestjs/config';
 
 config();
 
-const configService = new ConfigService();
-
 let dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: configService.get('DB_HOST'),
-  port: Number(configService.get('DB_PORT')),
-  username: configService.get('DB_USER'),
-  password: String(configService.get('DB_PASS')),
-  database: configService.get('DB_NAME'),
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: String(process.env.DB_PASS),
+  database: process.env.DB_NAME,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
 };
 
-if (configService.get('DB_SSL') === 'true') {
+if (process.env.DB_SSL === 'true') {
   dataSourceOptions = {
     ...dataSourceOptions,
     ssl: true,
